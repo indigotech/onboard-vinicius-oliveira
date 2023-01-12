ef7ded3c0c6f0a3d898deebc8ee068b914873fea
Vinicius Oliveira <v245247@dac.unicamp.br>
2023-01-12 12:33:42 -0300

    basic user query

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,5 +1,30 @@
+const users = [
+  {
+    id: 1,
+    name: 'Vinicius',
+    password: 'password123',
+  },
+  {
+    id: 2,
+    name: 'Shinji',
+    password: 'asuka321',
+  },
+];
+
 export const resolvers = {
   Query: {
-    hello: () => 'Hello World!',
+    hello: () => 'Hello World',
+    users: () => [
+      {
+        id: 1,
+        name: 'Vinicius',
+        password: 'password123',
+      },
+      {
+        id: 2,
+        name: 'Shinji',
+        password: 'asuka321',
+      },
+    ],
   },
 };
--- src/schema.ts
+++ src/schema.ts
@@ -1,8 +1,20 @@
 import { buildSchema } from 'graphql';
 
 export const typeDefs = buildSchema(`
+
 type Query {
-    hello: String!
+    hello: String
+    users: [User!]
+}
+
+type User {
+    id: ID!
+    name: String!
+    password: String!
+}
+
+type Mutation {
+    createUser(name: String, password: String): User!
 }
 
 `);
