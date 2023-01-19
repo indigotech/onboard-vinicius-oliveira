da66b10db83f45e1482050cfb776ac6d6f4df195
Vinicius Oliveira <v245247@dac.unicamp.br>
2023-01-19 16:28:24 -0300

    Token Auth to create User

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,11 +1,7 @@
 import { User } from './User';
 import { AppDataSource } from './data-source';
 import { CustomError } from './test/format-error';
-<<<<<<< HEAD
 import { checkEmail, checkPassword, checkToken, generateToken, passwordHashing, userRepository } from './utils';
-=======
-import jwt from 'jsonwebtoken';
->>>>>>> 8a6c25e (create token function)
 
 export const resolvers = {
   Query: {
@@ -61,34 +57,3 @@ export const resolvers = {
     },
   },
 };
-<<<<<<< HEAD
-=======
-
-const userRepository = AppDataSource.getRepository(User);
-
-function checkPassword(password: string) {
-  if (password.length < 6) {
-    throw new CustomError('Password must contain more than 6 characters', 401);
-  }
-
-  const regex = /([0-9].*[a-z])|([a-z].*[0-9])/;
-
-  if (!regex.test(password)) {
-    throw new CustomError('Password must contain at Least 1 Number and 1 Letter', 401);
-  }
-}
-
-async function checkEmail(inputEmail: string) {
-  if (await userRepository.findOneBy({ email: inputEmail })) {
-    throw new CustomError('This e-mail is alredy in use', 401);
-  }
-}
-
-export function passwordHashing(password: string) {
-  return crypto.createHash('sha256').update(password).digest('base64');
-}
-
-export function generateToken(userId: number, rememberMe: boolean) {
-  return jwt.sign({ userId: userId }, process.env.TOKEN_SECRET, { expiresIn: rememberMe === true ? '7d' : '1d' });
-}
->>>>>>> 8a6c25e (create token function)
